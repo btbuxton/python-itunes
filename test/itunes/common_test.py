0da1c7d5be1e0b6a7ae215380ff126f1972a92b8
Blaine Buxton <self@blainebuxton.com>
2012-09-09 10:08:15 -0400

    added more tests

--- test/itunes/common_test.py
+++ test/itunes/common_test.py
@@ -13,12 +13,28 @@ class CommonTest(unittest.TestCase):
         self.assertTrue(func_or(lambda: True, lambda: False)(), "T | F = T")
         self.assertTrue(func_or(lambda: False, lambda: True)(), "F | T = T")
         self.assertFalse(func_or(lambda: False, lambda: False)(), "F | F = F")
+        self.assertFalse(func_or()(), "F")
         
     def testFuncAnd(self):
         self.assertTrue(func_and(lambda: True, lambda: True)(), "T & T = T")
         self.assertFalse(func_and(lambda: True, lambda: False)(), "T & F = F")
         self.assertFalse(func_and(lambda: False, lambda: True)(), "F & T = F")
         self.assertFalse(func_and(lambda: False, lambda: False)(), "F & F = F")
+        self.assertTrue(func_and()(), "T")
+        
+    def testEveryDo(self):
+        result=[0]
+        def addOne():
+            result[0] += 1
+        action = every_do(2, addOne)
+        action()
+        self.assertEqual(0, result[0])
+        action()
+        self.assertEqual(1, result[0])
+        action()
+        self.assertEqual(1, result[0])
+        action()
+        self.assertEqual(2, result[0])
 
 if __name__ == "__main__":
     unittest.main()
